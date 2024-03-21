7c40e545db6fce292998cc474570f40dda773f3d
Marco.Andrey <marco.andrey1208@gmail.com>
2024-03-20 20:28:47 -0500

    Avance del curso

---
+++ proyectos/introduccionJS/js/02.js
@@ -0,0 +1,10 @@
+// No se puede reasignar
+
+const cliente = "Marco"
+cliente = 30
+
+
+// Tienen que tener un valor inicial forma correcta 
+
+const precio = 100
+console.log(precio)
---
+++ proyectos/introduccionJS/js/03.js
@@ -0,0 +1,46 @@
+// Tipos de datos en JavaScript
+
+// let cliente
+// console.log(cliente)
+// console.log(typeof cliente)
+
+// strings o cadenas de texto
+// const alumno = "Marco"
+// console.log(alumno)
+// console.log(typeof alumno)
+
+// Numbers
+// const numero1 = 20.20
+// const numero2 = 30
+// const numero3 = -100
+
+// console.log(typeof numero1)
+// console.log(typeof numero2)
+// console.log(typeof numero3)
+
+
+// BigInt
+// const numeroGrande = BigInt (42379832748723985663425963426523465786477895672367584648)
+// console.log(typeof numeroGrande)
+
+// Boolean
+// const descuento = false 
+// console.log(typeof descuento)
+
+// Null
+// const descuento = null 
+// console.log(descuento)
+
+// undefined
+// let precio 
+// console.log( precio)
+
+// Muy rara ves se utiliza asi 
+// let costo =  undefined
+// console.log( costo)
+
+// Symbol
+// const primerSymboll = Symbol(30)
+// const segundaSymboll = Symbol(30)
+// console.log(primerSymboll === segundaSymboll)
+    
