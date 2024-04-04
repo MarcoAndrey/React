56298282ff49ce066e8481ee242d4c8f20cb9e0b
Marco.Andrey <marco.andrey1208@gmail.com>
2024-04-04 12:31:56 -0500

    Avance curso parte 3

--- proyectos/introduccionJS/js/05.js
+++ proyectos/introduccionJS/js/05.js
@@ -6,3 +6,18 @@ const producto = {
   precio: 300,
   displonible: false,
 }
+
+// Object.freeze(producto)
+
+// Object.seal(producto)
+
+// Reescribir un valor
+producto.displonible = true
+
+// Si no existe, lo va agregar
+producto.imagen = 'imagen.jpg'
+
+// Eliminar propiedad 
+delete producto.precio
+
+console.log(producto)
---
+++ proyectos/introduccionJS/js/06.js
@@ -0,0 +1,22 @@
+// Objetos - Destructuring de dos o mas objetos 
+// primer objeto
+const producto = {
+  nombre: "Table",
+  precio: 300,
+  displonible: false,
+}
+// segundo objeto
+const cliente = {
+    nombre: "marco",
+    premium: true,
+    direccion: {
+        calle:"Bogota"
+    }
+}
+
+const {nombre} = producto
+const {nombre: nombreCliente, direccion: { calle }} = cliente
+
+console.log(nombre)
+console.log(nombreCliente)
+console.log(calle)
