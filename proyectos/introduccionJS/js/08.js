cac552a0e720b1d89b7b45818b586acfc69ed714
Marco.Andrey <marco.andrey1208@gmail.com>
2024-04-05 18:10:34 -0500

    Avance curso parte 4

---
+++ proyectos/introduccionJS/js/08.js
@@ -0,0 +1,11 @@
+// Template strings y concatenacion
+
+const producto = "Tablet de 12 pulgadas"
+const precio = 400
+const marca = "Orange"
+
+console.log('El producto es: ' + producto)
+console.log(`El producto es: ${producto}`)
+
+console.log (producto + " $ " + precio + " Dolares, Marca: " + marca)
+console.log(`${producto} $${precio} Dolares, Marca: ${marca}`)
