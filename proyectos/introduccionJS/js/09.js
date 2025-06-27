55ed760e76cc663b9007ee06700ac2c02ea98b23
Marco.Andrey <marco.andrey1208@gmail.com>
2025-06-26 22:10:26 -0500

    class #9

---
+++ proyectos/introduccionJS/js/09.js
@@ -0,0 +1,20 @@
+// Arrays o areglos
+// Un arrays es un objeto similar a un listado; una vez que genera un arreglo, nos va a dar ciertos métodos
+// para efectuar operaciones de recorrer ese arreglo o de modificar u otros métodos para realizar operaciones específicas
+// la sintaxis es muy parecida a la de los objetos
+// Un array es un objeto que permite almacenar varios valores en una sola variable
+
+// ------------------------------------------------------------------------------------------------------------------------
+
+const tecnologias = [20, 30, 40];
+console.table(tecnologias);
+
+// para acceder a un valor de un array se utiliza el indice asi
+console.log(tecnologias[0]); // 20
+console.log(tecnologias[1]); // 30
+console.log(tecnologias[2]); // 40
+
+// ------------------------------------------------------------------------------------------------------------------------
+// Los arreglos en JavaScript pueden contener cualquier tipo de dato
+
+const tecnologias = [20, 30, 40, true, "react,js"];
